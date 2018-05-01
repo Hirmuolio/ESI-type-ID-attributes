1861488018b4e6a2d54364c1b07f16a5066376d7
Hirmuolio <hirmuolio@gmail.com>
2018-05-01 13:51:23 +0300

    More errors, less crashes.

--- npcinfo.py
+++ npcinfo.py
@@ -5,36 +5,47 @@
 import requests
 import json
 
-try:
-	#Load cached dogma attribute ID info
-    attributes = json.load(open('attributes.txt'))
-except FileNotFoundError:
-	#No file found. Start from scratch
-    attributes = {}
+def check_error(esi_response, job):
+	status_code = esi_response.status_code
+	
+	if status_code != 200:
+		#Error
+		print('Failed to '+job+'. Error',esi_response.status_code,'-', esi_response.json()['error'])
+		error = True
+	else:
+		error = False
+		try:
+			#Try to print warning
+			print('Warning',esi_response.headers['warning'])
+		except KeyError:
+			warning = False
+	
+	return error
 
-while True:
-	#Call ESI
-	type_ID = input("Give type ID: ")
-
-	Url = "https://esi.tech.ccp.is/v3/universe/types/"+type_ID+"/?datasource=tranquility&language=en-us"
-
-	#Uncomment this to get SISI stats
-	#Url = "https://esi.tech.ccp.is/v3/universe/types/"+type_ID+"/?datasource=singularity&language=en-us"
-
-	esi_response = requests.get(Url)
+def parse_stats(esi_response):
 	npc_Stats = esi_response.json()
 
-	#Find out what each of the dogma IDs mean
-	length = len(npc_Stats['dogma_attributes'])
+	#Check if it has attributes.
+	#If it does do the things
+	try:
+		length = len(npc_Stats['dogma_attributes'])
+	except KeyError:
+		print('Type ID',npc_Stats['type_id'],'has no attributes')
+		return
 
 	for n in range(0, length):
 		dogma_id = npc_Stats['dogma_attributes'][n]['attribute_id']
 		if not str(dogma_id) in attributes:
 			#Find what this ID is for
 			print('Getting info on dogma attribute ID', dogma_id)
-			Url = "https://esi.tech.ccp.is/v1/dogma/attributes/"+str(dogma_id)+"/?datasource=tranquility"
-			attributes[str(dogma_id)] = [ requests.get(Url).json()['name'], requests.get(Url).json()['display_name'], requests.get(Url).json()['description'] ]
-
+			url = "https://esi.tech.ccp.is/v1/dogma/attributes/"+str(dogma_id)+"/?datasource=tranquility"
+			esi_response = requests.get(url)
+			
+			if not check_error(esi_response, 'get info on dogma attribute'):
+				response_json = esi_response.json()
+				attributes[str(dogma_id)] = [ response_json['name'], response_json['display_name'], response_json['description'] ]
+			
+			
 	#Save the ID list
 	with open('attributes.txt', 'w') as outfile:
 		json.dump(attributes, outfile)
@@ -55,4 +66,29 @@ while True:
 		description = attributes[str(dogma_id)][2]
 		print( '{:<30s} {:<10s} {:<}{:<}'.format(name, str(value), '( '+display_name, ', '+description+' )'))
 	print('----\n')
+	return
+
+try:
+	#Load cached dogma attribute ID info
+    attributes = json.load(open('attributes.txt'))
+except FileNotFoundError:
+	#No file found. Start from scratch
+    attributes = {}
+
+while True:
+	#Call ESI
+	type_ID = input("Give type ID: ")
+
+	Url = "https://esi.tech.ccp.is/v3/universe/types/"+type_ID+"/?datasource=tranquility&language=en-us"
+
+	#Uncomment this to get SISI stats
+	#Url = "https://esi.tech.ccp.is/v3/universe/types/"+type_ID+"/?datasource=singularity&language=en-us"
+
+	esi_response = requests.get(Url)
+	
+	if not check_error(esi_response, 'get attributes for type ID'):
+		parse_stats(esi_response)
+	
+	
+	
 	
